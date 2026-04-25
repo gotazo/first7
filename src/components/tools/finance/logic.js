1cf8bef72c3b6aa07c3365beda4e8d9f766f163e
Kedusal Elias <kuovi@proton.me>
2026-04-25 12:43:36 +0530

    Remove unused finance JS files (now using inline script)

--- src/components/tools/finance/logic.js
+++
@@ -1,9 +0,0 @@
-export function calculate(income, includeFuture) {
-  const tithe = income * 0.10;
-  const help = income * 0.05;
-  const emergency = income * 0.05;
-  const future = includeFuture ? income * 0.10 : 0;
-  const living = income - (tithe + help + emergency + future);
-
-  return { income, tithe, help, emergency, future, living };
-}
